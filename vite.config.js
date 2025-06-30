3883075b81d1f16eb65ea1602a478165911a843c
Alves1986 <cassia.andinho@gmail.com>
2025-06-29 23:30:10 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: 3000,
+    host: '0.0.0.0'
+  }
+})
+
