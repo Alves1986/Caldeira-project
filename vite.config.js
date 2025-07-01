bc6543c601e6df238aec65fddb224f347a573996
Alves1986 <cassia.andinho@gmail.com>
2025-07-01 20:14:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Alves1986/Caldeira-project/',
+  base: '/Caldeira-project/',
 });
 
